d2f31c154bdc832bc5f2589cae5b298340b501d9
larisa <larisa.mih2004@gmail.com>
2024-02-18 22:33:26 +0300

    written script in main.py for homework-1

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,31 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+import csv
+
+with psycopg2.connect(host='localhost', database='north', user='postgres', password="alesar") as conn:
+    with conn.cursor() as cur:
+        with open('north_data/employees_data.csv', 'r') as f:
+            reader = csv.reader(f)
+            next(reader)
+            for row in reader:
+                cur.execute(
+                    "insert into employees(employee_id,first_name,last_name,title,birth_date,notes) values(%s,%s,%s,"
+                    "%s,%s,%s)",
+                    row)
+        with open('north_data/customers_data.csv', 'r') as f:
+            reader = csv.reader(f)
+            next(reader)
+            for row in reader:
+                cur.execute(
+                    "insert into customers(customer_id,company_name,contact_name) values(%s,%s,%s)",
+                    row)
+
+        with open('north_data/orders_data.csv', 'r') as f:
+            reader = csv.reader(f)
+            next(reader)
+            for row in reader:
+                cur.execute(
+                    "insert into orders(order_id,customer_id,employee_id,order_date,ship_city) values(%s,%s,%s,%s,%s)",
+                    row)
+
+conn.close()
